4d7932c6cf234382307618cb3656e4702e1924db
SahilArdeshna <sahilardeshna@gmail.com>
2025-07-29 22:17:36 +0530

    added print

--- indicators/indicators.py
+++ indicators/indicators.py
@@ -18,6 +18,7 @@ def fetch_ohlcv(symbol, interval=None, limit=100):
     try:
         url = f"https://api.binance.com/api/v3/klines?symbol={symbol}&interval={interval}&limit={limit}"
         res = requests.get(url).json()
+        print('res', res)
         df = pd.DataFrame(res, columns=[
             "open_time", "open", "high", "low", "close", "volume", "close_time", "quote_asset_volume", "number_of_trades", "taker_buy_base_asset_volume", "taker_buy_quote_asset_volume", "ignore"
         ])
